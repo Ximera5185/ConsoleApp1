e6a42ab588476d4ddf9b50eb1d43f0ad0ca66693
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2023-11-16 04:43:20 +0300

    1

--- Arena.cs
+++ Arena.cs
@@ -8,33 +8,85 @@ namespace ConsoleApp1
 {
     internal class Arena
     {
-        List<Person> fighters = new List<Person>();
+        private readonly List<Person> _fighters = new List<Person>();
 
         public void StartArena()
         {
-            fighters.Add(new BabaYga());
-            fighters.Add(new Dragon());
-            fighters.Add(new Goblin());
-            fighters.Add(new Koschei());
-            fighters.Add(new WaterMan());
+            Person figterOne;
+            Person fighterTwo;
+            
+            int firstFighter;
+            int secondFighter;
+
+            string messageOneSelect = "Выберите первого бойца";
+            string messageTwoSelect = "Выберите второго бойца";
+
+            AddFightersList();
+
+            firstFighter = SelectFighters(messageOneSelect);
+
+            figterOne = CreateFighters(firstFighter);
+
+            secondFighter = SelectFighters(messageTwoSelect);
+
+            fighterTwo = CreateFighters(secondFighter);
+
+            StartFight(figterOne,fighterTwo);
+        }
+
+        private Person CreateFighters(int numberFighter)
+        {
+            numberFighter--;
+
+            for (int i = 0; i < _fighters.Count; i++)
+            {
+                if (i == numberFighter)
+                {
+                    return _fighters [i];
+                }
+                
+            }
+
+            return null;
+        }
+
+        private void AddFightersList()
+        {
+            _fighters.Add(new BabaYga());
+            _fighters.Add(new Dragon());
+            _fighters.Add(new Goblin());
+            _fighters.Add(new Koschei());
+            _fighters.Add(new WaterMan());
+        }
+
+        private void ShowListFighters()
+        {
+            int serialNumber = 1;
 
             Console.WriteLine("Список бойцов");
 
-            foreach (Person person in fighters)
+            for (int i = 0; i < _fighters.Count; i++)
             {
-                Console.WriteLine($"Имя - {person.Name} Здоровье - {person.Health} Дамаг - {person.Damage}");
+                Console.WriteLine($"{serialNumber} - {_fighters [i].Name} Здоровье - {_fighters [i].Health} Дамаг - {_fighters [i].Damage}");
+
+                serialNumber++;
             }
         }
 
-        private void CreateFighters()
+        private int SelectFighters(string messageSelect)
         {
-            fighters.Add(new BabaYga());
+            Console.WriteLine(messageSelect);
 
+            ShowListFighters();
+
+            return Convert.ToInt32(Console.ReadLine());
         }
 
-        private void AddListFighters()
+        private Person StartFight(Person figterOne, Person fighterTwo) 
         {
+            Person vinner = null;
 
+            return vinner;
         }
     }
 }
